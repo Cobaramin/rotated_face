fc40900b428b4669a077d71ff7ebd537e0a4caf2
cobaramin <gamekansompoj@gmail.com>
2019-05-03 04:34:02 +0700

    add gunicorn add edit dockerfile

--- web/run.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from web.app import app as application
-
-
-if __name__ == '__main__':
-    application.config.from_object('web.config.{}'.format(os.environ.get('FLASK_CONFIG', 'development').capitalize()))
-    application.run(host=application.config['HOST'], port=application.config['PORT'], debug=application.config['DEBUG'])
